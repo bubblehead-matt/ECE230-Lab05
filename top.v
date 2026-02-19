e11d200c723808479a5aa7a857a20631d726938a
Matt DeRoos <MattDeRoos@u.boisestate.edu>
2026-02-18 22:03:22 -0700

    top working

--- top.v
+++ top.v
@@ -1,1 +1,25 @@
 // Implement top level module
+module top (
+    input [6:0] sw,
+    output [1:0] led
+);
+
+    // Circuit a
+    circuit_a (
+      .A(sw[0]),
+      .B(sw[1]),
+      .C(sw[2]),
+      .D(sw[3]),
+      .Y(cir[b]) // TODO connect to circuit b
+    );
+
+    // Circuit b
+    circuit_b (
+      .A(cir[a]), // TODO connect from circuit a
+      .B(sw[4]),
+      .C(sw[5]),
+      .D(sw[6]),
+      .Y(led[1])
+    );
+
+endmodule
